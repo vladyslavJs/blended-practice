e2c08a12d300857297fc77eec2dc344504001ab4
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-25 17:41:09 +0100

    Update arrow-methods.js

--- js/arrow-methods.js
+++ js/arrow-methods.js
@@ -39,41 +39,45 @@ console.table(users);
 // виведення даних циклом for or, для масивів
 
 //-------------------------------------
+// метод forEach - це спрощення методу for of
+// Не повертає нічого, лише значення undefined. Може мутувати оригінальний масив.
 // users.forEach(function (item, index) {
 //     console.log(item);
 //     console.log(index);
 // });
-// метод forEach - це спрощення методу for of
 
-// const count = {
-//     men: 0,
-//     women: 0,
-// }
+// Задача.
+const count = {
+    men: 0,
+    women: 0,
+}
 
-// users.forEach(function (element, index) {
-//     element.sex === 'male' ? count.men++ : count.women++;
-// });
+users.forEach(function (element, index) {
+    element.sex === 'male' ? count.men++ : count.women++;
+});
 
-// console.log(count);
-
-//-------------------------------------
-// const adultsUsers = users.filter(function (item) {
-//     return item.age >= 18;
-// });
-// метод filter
-
-// console.log(adultsUsers);
+console.log(count);
 
 //-------------------------------------
+// Метод filter.
+const adultsUsers = users.filter(function (item) {
+    return item.age >= 18;
+});
+
+console.log(adultsUsers);
+
+//-------------------------------------
+// Метод map
+// Проходиться по заданому масиві, модифікуючи всі його записи в залежності від умови в дужках. Не змінює оригінальний масив. Повертає створений новий масив із зміненими даними.
 const correctNames = users.map(function (item) {
     return item.sex === 'male' ? 'Mr. ' + item.name : 'Mrs. ' + item.name;
 });
 
-
-// метод map
+console.log(correctNames);
 
 //-------------------------------------
 // Метод reduce.Принцип дії
+// Метод reduce() по черзі перебирає значення елементів масиву, зберігаючи проміжний результат своєї обробки, за допомогою накопичувача (acc) та початковим значенням(currentValue).
 // Акамулятор, іншими словами накопичувач.currentValue - поточне значення.Відклік починається з 0. 
 // Нам потрібно вивести вік користувачів, а саме до накопичувача додавати поточні значення і так по кожному об'єкту 
 // 1. accamulator = 0, currentValue = 26
@@ -89,15 +93,15 @@ const sum = users.reduce(function (accmulator, currentValue) {
 console.log(sum / users.length);
 
 //-------------------------------------
-// Метод find. Шукає елемент в масиві.
+// Метод find. Шукає елемент в масиві. Якщо елемент не знайдено, портає значення undefined.
 const foundObj = users.find(function (item) {
-    return item.name === 'Vitaly';
+    return item.name === 'Rob';
 });
 
 console.log(foundObj);
 
 //-------------------------------------
-// Метод some. Перевіряє чи хоч одна умова задовілняється.
+// Метод some. Перевіряє чи хоч одна умова задовілняється.Повертає істинність, якщо знаходить в масиві елемент, для якого надана функція повертає істинність, інакше - хибність. Він не змінює масив.
 const foundObject = users.some(function (item) {
     return item.age > 18;
 });
@@ -105,7 +109,7 @@ const foundObject = users.some(function (item) {
 console.log(foundObject);
 
 //-------------------------------------
-// Метод every. Перевіряє чи кожна умова задовілняється.
+// Метод every. Перевіряє чи кожна умова задовілняється.Повертає істинність, якщо знаходить в масиві елемент, для якого надана функція повертає істинність, інакше - хибність. Він не змінює масив.
 const found = users.every(function (item) {
     return item.age  >= 16;
 });
