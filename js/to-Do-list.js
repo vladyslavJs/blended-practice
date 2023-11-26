3dd2bb3cb8afef875fc4b3d9d548620c10ab5841
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-26 20:32:20 +0100

    Create to-Do-list.js

---
+++ js/to-Do-list.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('.input-container button');
+const input = document.querySelector('.input-container input');
+const list = document.querySelector('.todo-list');
+
+//call-back function
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.className = 'todo-list-item';
+    li.innerText = input.value;
+    
+    const deleteBt = document.createElement('button');
+    deleteBt.innerText = 'Delete';
+    li.appendChild(deleteBt);
+    list.appendChild(li);
+    input.value = '';
+
+    deleteBt.addEventListener('click', () => {
+        list.removeChild(li);
+    })
+});
+
+// console.log(button);
+// console.log(input);
+// console.log(list);
